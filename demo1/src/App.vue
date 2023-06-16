8354299a4879240e4b6a7f34389fb0830760d674
boydzheng <boydzheng@tencent.com>
2023-06-16 13:56:07 +0800

    add

--- demo1/src/App.vue
+++ demo1/src/App.vue
@@ -1,20 +1,109 @@
-<template>
-  <div class="login-container">
-    <el-input v-model="input" placeholder="Please input" clearable />
-    <el-input v-model="input" type="password" placeholder="Please input password" show-password />
+<!-- <template> -->
+  <!-- <div class="login_input">
+    <el-input style="line-height: 50px;" v-model="inputUsername" placeholder="Please input username" clearable />
   </div>
-</template>
+  <div class="login_input">
+    <el-input v-model="inputPwd" type="password" placeholder="Please input password" show-password />
+  </div> -->
+
+  <!-- <div style="height: 50px; display: flex; justify-content: center; align-items: center;">
+    <el-input v-model="inputUsername" placeholder="请输入内容" style="line-height: 50px;"></el-input>
+  </div> -->
+
+  <!-- </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-const input = ref('')
+const inputUsername = ref('')
+// const inputPwd = ref('')
 </script>
 
 <style >
-.login-container {
-  width: 100%;
-  min-height: 100%;
-  overflow: hidden;
-  background-color: #2d3a4b;
+#login_input {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+}
+</style> -->
+
+<template>
+  <el-row>
+    <el-col :span="24">
+      <div class="grid-content ep-bg-purple-dark" />
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="12">
+      <div class="grid-content ep-bg-purple" />
+    </el-col>
+    <el-col :span="12">
+      <div class="grid-content ep-bg-purple-light" />
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="8">
+      <div class="grid-content ep-bg-purple" />
+    </el-col>
+    <el-col :span="8">
+      <div class="grid-content ep-bg-purple-light" />
+    </el-col>
+    <el-col :span="8">
+      <div class="grid-content ep-bg-purple" />
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="6">
+      <div class="grid-content ep-bg-purple" />
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content ep-bg-purple-light" />
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content ep-bg-purple" />
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content ep-bg-purple-light" />
+    </el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="4">
+      <div class="grid-content ep-bg-purple" />
+    </el-col>
+    <el-col :span="4">
+      <div class="grid-content ep-bg-purple-light" />
+    </el-col>
+    <el-col :span="4">
+      <div class="grid-content ep-bg-purple" />
+    </el-col>
+    <el-col :span="4">
+      <div class="grid-content ep-bg-purple-light" />
+    </el-col>
+    <el-col :span="4">
+      <div class="grid-content ep-bg-purple" />
+    </el-col>
+    <el-col :span="4">
+      <div class="grid-content ep-bg-purple-light" />
+    </el-col>
+  </el-row>
+</template>
+
+<style lang="scss" >
+.el-row {
+  margin-bottom: 20px;
+}
+
+.el-row:last-child {
+  margin-bottom: 0;
+}
+
+.el-col {
+  color: red;
+  border-radius: 4px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
 }
 </style>
