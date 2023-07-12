4ccdf90ae5bc9875ec294f168c6e878fc245986c
boydzheng <boydzheng@tencent.com>
2023-07-12 17:40:53 +0800

    add

--- vue3+ts+elementui-plus/src/App.vue
+++ vue3+ts+elementui-plus/src/App.vue
@@ -1,25 +1,37 @@
-<script setup lang="ts">
-import {
-  Check,
-  Delete,
-  Edit,
-  Message,
-  Search,
-  Plus,
-} from '@element-plus/icons-vue'
-</script>
-
 <template>
   <div>
     <div>
       boya
     </div>
 
+    <hr>
     <el-button type="primary" size="default" :icon="Plus" @click="">主要按钮</el-button>
     <el-button type="success" size="small" :icon="Edit" @click=""></el-button>
     <el-button type="danger" size="default" :icon="Delete" @click=""></el-button>
+
+    <hr>
+    <div>svg测试</div>
+    <svgIcon iconName="shouji" :size="200"></svgIcon>
+
 
   </div>
 </template>
 
 <style scoped></style>
+
+<script setup lang="ts">
+import {
+  Delete,
+  Edit,
+  Plus,
+} from '@element-plus/icons-vue'
+
+import { defineComponent } from 'vue'
+import SvgIcon from './components/SvgIcon.vue'
+
+defineComponent({
+  name: 'App',
+  components: { SvgIcon },
+})
+
+</script>
--- vue3+ts+elementui-plus/src/main.ts
+++ vue3+ts+elementui-plus/src/main.ts
@@ -4,6 +4,9 @@ import App from './App.vue'
 // 引用element-plus和样式
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+// 导入svg插件配置代码
+import 'virtual:svg-icons-register'
+
 // 获取应用实例对象
 const app = createApp(App)
 app.use(ElementPlus)
