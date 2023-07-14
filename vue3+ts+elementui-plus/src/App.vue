1419473855a0da4875660e7ca17e847c7262bfd3
boydzheng <boydzheng@tencent.com>
2023-07-14 21:19:01 +0800

    add

--- vue3+ts+elementui-plus/src/App.vue
+++ vue3+ts+elementui-plus/src/App.vue
@@ -1,28 +1,8 @@
 <template>
-  <div>
-    <div>
-      boya
-    </div>
-
-    <hr>
-    <el-button type="primary" size="default" :icon="Plus" @click="">主要按钮</el-button>
-    <el-button type="success" size="small" :icon="Edit" @click=""></el-button>
-    <el-button type="danger" size="default" :icon="Delete" @click=""></el-button>
-
-    <hr>
-    <div>svg测试</div>
-    <svgIcon iconName="shouji" :size="200"></svgIcon>
-
-
-  </div>
+  <router-view></router-view>
 </template>
 
 <style scoped></style>
 
 <script setup lang="ts">
-import {
-  Delete,
-  Edit,
-  Plus,
-} from '@element-plus/icons-vue'
 </script>
---
+++ vue3+ts+elementui-plus/src/views/login/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="login-container">
+        <el-row class="login-bg">
+            <el-col>
+                <!-- 登入表单 -->
+                <el-form class="login-form" size="normal">
+                    <h1>Hello</h1>
+                    <h2>欢迎来到这里</h2>
+                    <el-form-item label="">
+                        <el-input type="primary"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-input type="password"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary">登入</el-button>
+                    </el-form-item>
+                </el-form>
+
+            </el-col>
+        </el-row>
+
+    </div>
+</template>
+    
+<script setup lang='ts'>
+
+</script>
+    
+<style scoped lang="scss">
+.login-container {
+    width: 100%;
+    height: 100vh;
+    background: url('@/assets/images/background.jpg') no-repeat;
+    background-size: cover;
+
+    .login-bg {
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+
+        // padding: 40px;
+        // background-color: gray;
+        .login-form {
+            position: relative;
+            width: 80%;
+            padding: 40px;
+            background: url("@/assets/images/login_form.png") no-repeat;
+            background-size: cover;
+        }
+    }
+}
+</style>
